236bf30625156592da637d01d3208bf4f71e84da
Pavel Sinegubov <sineguboff@gmail.com>
2024-04-17 22:52:15 +0700

    Add some negative tests and fill README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+1. test_add_new_book_name_too_short_empty_books_genre - Негативный тест на неуспешным добавлением в список книг, книги 
+с нулевым именем.
+2. test_add_new_book_name_too_long_empty_books_genre - Негативный тест на неуспешным добавлением в список книг, книги 
+с  именем больше 41 символа.
+3. test_add_new_book_add_two_books - Проверка, что добавленные 2 книги увеличивают словарь books_genre.
+4. test_add_new_book_valid_name  - Проверка добавления книг с валидными именами в словарь books_genre.
+5. test_set_book_genre - Проверка добавления книги и жанра в словарь books_genre.
+6. test_set_book_genre_not_valid_genre_empty_genre - Негативная проверка на добавление книги с не поддерживаемым жанром.
--- test_main.py
+++ test_main.py
@@ -8,10 +8,13 @@ class TestBooksCollector:
     def books(self):
         return BooksCollector()
 
-    @pytest.mark.parametrize('book_name',
-                             ['', 'A'*42]
-                             )
-    def test_add_new_book_add_incorrect_name_not_added(self, book_name, books):
+    def test_add_new_book_name_too_short_empty_books_genre(self, books):
+        book_name = ""
+        books.add_new_book(book_name)
+        assert len(books.get_books_genre()) == 0
+
+    def test_add_new_book_name_too_long_empty_books_genre(self, books):
+        book_name = "a" * 42
         books.add_new_book(book_name)
         assert len(books.get_books_genre()) == 0
 
@@ -20,8 +23,13 @@ class TestBooksCollector:
         books.add_new_book("Евгений Онегин")
         assert len(books.get_books_genre()) == 2
 
-    def test_add_new_book(self, books):
-        book_name = "Peppa the pig"
+    @pytest.mark.parametrize('book_name', [
+        'Тихий Дон',
+        'Макс Фрай',
+        'Учебник по химии'
+    ]
+                             )
+    def test_add_new_book_valid_name(self, books, book_name):
         books.add_new_book(book_name)
         assert book_name in books.books_genre
 
@@ -35,7 +43,7 @@ class TestBooksCollector:
         ('10 негритят', 'Детективы'),
         ('Дракула', 'Ужасы'),
         ('12 Стульев', 'Комедии'),
-        ('Властелин колец', 'Фантастика')
+        ('20 тысяч лье под водой', 'Фантастика')
         ]
                              )
     def test_set_book_genre(self, books, book_name, genre):
@@ -43,16 +51,41 @@ class TestBooksCollector:
         books.set_book_genre(book_name, genre)
         assert books.get_book_genre(book_name) == genre
 
-    @pytest.mark.parametrize('book_name, genre', [
-        ('StarTrack', 'Научная фантастика'),
-        ('Алгебра', 'Учебники'),
-        ('Железный человек', 'Комиксы'),
-        ]
-                             )
-    def test_set_book_genre_not_valid_genre(self, books, book_name, genre):
+    def test_set_book_genre_not_valid_genre_empty_genre(self, books):
+        book_name = 'Человек Паук'
+        genre = 'Комиксы'
         books.add_new_book(book_name)
         books.set_book_genre(book_name, genre)
         assert not books.get_book_genre(book_name) == genre
+
+    def test_get_books_with_specific_genre(self, books):
+        books.add_new_book("Хоббит")
+        books.add_new_book("Дюна")
+        books.set_book_genre("Хоббит", "Фантастика")
+        books.set_book_genre("Дюна", "Фантастика")
+        assert books.get_books_with_specific_genre("Фантастика") == ["Хоббит", "Дюна"]
+
+    @pytest.mark.parametrize('book_name, genre', [
+        ('Буратино', 'Мультфильмы'),])
+    def test_get_books_with_specific_genre_2(self, books, book_name, genre):
+        books.add_new_book(book_name)
+        books.set_book_genre(book_name, genre)
+        assert genre in books.genre
+
+    def test_get_books_with_specific_genre_missing_book(self, books):
+        assert len(books.get_books_with_specific_genre('Приключения')) == 0
+
+    def test_get_books_for_children(self, books):
+        books.add_new_book("Book6")
+        books.set_book_genre("Book6", "Мультфильмы")
+        assert "Book6" in books.get_books_for_children()
+    @staticmethod
+    def delete_book_from_favorites_true(books):
+        book_name = "Сборник стихов"
+        books.add_new_book(book_name)
+        books.add_book_in_favorites(book_name)
+        books.delete_book_from_favorites(book_name)
+        assert books.get_list_of_favorites_books() == []
 
     @pytest.mark.parametrize("book_name", ["Гарри Поттер", "Каштанка", "Звездные войны"])
     def test_add_book_in_favorites_name_contained_in_favorites_true(self, books, book_name):
